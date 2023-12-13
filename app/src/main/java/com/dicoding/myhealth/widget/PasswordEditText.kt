4b49ffcd1e53c37b3c6161ac481af36361d79a81
syaifa21 <alisyaifarudin04@gmail.com>
2023-12-13 19:34:05 +0700

    log

---
+++ app/src/main/java/com/dicoding/myhealth/widget/PasswordEditText.kt
@@ -0,0 +1,40 @@
+package com.dicoding.myhealth.widget
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+
+class PasswordEditText: AppCompatEditText {
+    constructor(context: Context): super(context){
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    private fun init() {
+        addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                error = if(p0.toString().length < 1){
+                    "Kata sandi tidak boleh kurang dari 8 karakter"
+                }else{
+                    null
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+    }
+}
