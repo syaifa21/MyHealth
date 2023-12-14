0d4435622309dc095f9bb84a3248b9914341f4d6
syaifa21 <alisyaifarudin04@gmail.com>
2023-12-14 15:03:04 +0700

    log

---
+++ app/src/main/java/com/dicoding/myhealth/api/response/RegisterResponse.kt
@@ -0,0 +1,9 @@
+package com.dicoding.myhealth.api.response
+
+import com.google.gson.annotations.SerializedName
+
+data class RegisterResponse(
+
+    @field:SerializedName("message")
+    val message: String? = null
+)
