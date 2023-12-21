6d0aff95582e1b3e38b9e1e250c711e76d2771c2
syaifa21 <alisyaifarudin04@gmail.com>
2023-12-21 22:00:33 +0700

    aa

--- app/src/main/java/com/dicoding/myhealth/ProfileActivity.kt
+++ app/src/main/java/com/dicoding/myhealth/ProfileActivity.kt
@@ -1,5 +1,7 @@
 package com.dicoding.myhealth
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.myhealth.databinding.ActivityLoginBinding
@@ -7,9 +9,15 @@ import com.dicoding.myhealth.databinding.ActivityProfileBinding
 
 class ProfileActivity: AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
+    lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.logout.setOnClickListener{
+            sharedPreferences.edit().clear().apply()
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/dicoding/myhealth/api/ApiServiceBMI.kt
+++ app/src/main/java/com/dicoding/myhealth/api/ApiServiceBMI.kt
@@ -5,14 +5,17 @@ import com.dicoding.myhealth.api.response.BMIResponse
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface ApiServiceBMI {
-    @FormUrlEncoded
+    @Multipart
     @POST("hitung-kalori")
-    fun signin(
-        @Field("username") name: String,
-        @Field("password") password: String
+    fun submituser(
+        @Part("berat") berat: Int,
+        @Part("tinggi") tinggi: Int,
+        @Part("umur") umur: Int,
+        @Part("gender") gender:String
     ): Call<BMIResponse>
-
 }
