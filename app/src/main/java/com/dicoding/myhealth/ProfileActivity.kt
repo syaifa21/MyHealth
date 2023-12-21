75f64807e388349873b961b6b348bcb43dce02b9
syaifa21 <alisyaifarudin04@gmail.com>
2023-12-21 23:04:13 +0700

    aa

--- app/src/main/java/com/dicoding/myhealth/ProfileActivity.kt
+++ app/src/main/java/com/dicoding/myhealth/ProfileActivity.kt
@@ -3,8 +3,9 @@ package com.dicoding.myhealth
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.dicoding.myhealth.databinding.ActivityLoginBinding
+
 import com.dicoding.myhealth.databinding.ActivityProfileBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -17,9 +18,8 @@ class ProfileActivity: AppCompatActivity() {
         setContentView(binding.root)
         sharedPreferences = getSharedPreferences("user", MODE_PRIVATE)
         binding.logout.setOnClickListener{
-            sharedPreferences.edit().clear().apply()
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
+            showYesNoDialog()
+
         }
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navbar)
         bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
@@ -36,5 +36,38 @@ class ProfileActivity: AppCompatActivity() {
                 else -> false
             }
         }
+    }
+    private fun showYesNoDialog() {
+        val builder = AlertDialog.Builder(this)
+
+        // Atur judul dan pesan dialog
+        builder.setTitle("Confirmation")
+            .setMessage("Are you sure you want to continue?")
+
+        // Atur tombol positif (Yes)
+        builder.setPositiveButton("Yes") { dialog, which ->
+            // Aksi yang diambil jika pengguna memilih Yes
+            // Misalnya, Anda dapat menambahkan kode untuk melanjutkan dengan operasi yang diinginkan
+            // ...
+            sharedPreferences.edit().clear().apply()
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+            // Tutup dialog setelah menyelesaikan aksi
+            dialog.dismiss()
+        }
+
+        // Atur tombol negatif (No)
+        builder.setNegativeButton("No") { dialog, which ->
+            // Aksi yang diambil jika pengguna memilih No
+            // Misalnya, Anda dapat menambahkan kode untuk menghentikan atau membatalkan operasi yang diinginkan
+            // ...
+
+            // Tutup dialog setelah menyelesaikan aksi
+            dialog.dismiss()
+        }
+
+        // Tampilkan dialog
+        val dialog = builder.create()
+        dialog.show()
     }
 }
