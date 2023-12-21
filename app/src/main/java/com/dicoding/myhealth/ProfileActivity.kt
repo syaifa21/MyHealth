a8d1c6f1965b5cedf1d0c84441d9046d5e3195ba
dml17 <ikhsandawamul@gmail.com>
2023-12-21 22:50:37 +0700

    a

--- app/src/main/java/com/dicoding/myhealth/ProfileActivity.kt
+++ app/src/main/java/com/dicoding/myhealth/ProfileActivity.kt
@@ -1,9 +1,11 @@
 package com.dicoding.myhealth
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.myhealth.databinding.ActivityLoginBinding
 import com.dicoding.myhealth.databinding.ActivityProfileBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ProfileActivity: AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
@@ -11,5 +13,21 @@ class ProfileActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navbar)
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> {
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                    true
+                }
+                R.id.profile -> {
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
